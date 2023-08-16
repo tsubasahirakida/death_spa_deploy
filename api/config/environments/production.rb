96d6aa423a61f368d77f4982347cf2e057bbdf80
hirakida <dantuba19990326@gmail.com>
2023-08-16 23:27:44 +0900

    [fix] dockerデプロイ

--- api/config/environments/production.rb
+++ api/config/environments/production.rb
@@ -83,10 +83,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.assets.compile = true
-  config.public_file_server.enabled = true
-
-  # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
   # to send a subsequent read to the primary.
